a008659a940b67ec2f02260b2d8d049fd4e529ff
parsher <futureslabor@gmail.com>
2019-10-14 22:40:47 +0900

    filters

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,45 @@ import './screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/filters_screen.dart';
+import './dummy_data.dart';
+import './models/meal.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] && !meal.isGlutenFree) {
+          return false;
+        } else if (_filters['lactose'] && !meal.isLactoseFree) {
+          return false;
+        } else if (_filters['vegan'] && !meal.isVegan) {
+          return false;
+        } else if (_filters['vegetarian'] && !meal.isVegetarian) {
+          return false;
+        } 
+        return true;
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,18 +69,20 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (ctx) => TabScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        CategoryMealsScreen.routeName: (ctx) =>
+            CategoryMealsScreen(_availableMeals),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-        FiltersScreen.routeName: (ctx) => FiltersScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters),
       },
       // onGenerateRoute: (settings) {
-        // if (settings.name == '/meal-detail') {
-        // }
-        // settings.arguments
-        //return MaterialPageRoute(builder: (ctx) => CategoryMealsScreen());
+      // if (settings.name == '/meal-detail') {
+      // }
+      // settings.arguments
+      //return MaterialPageRoute(builder: (ctx) => CategoryMealsScreen());
       // },
       onUnknownRoute: (settings) {
-        return MaterialPageRoute(builder: (ctx) => CategoryMealsScreen());
+        return MaterialPageRoute(
+            builder: (ctx) => CategoryMealsScreen(_availableMeals));
       },
     );
   }
